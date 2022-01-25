fe85b32254c1c90d7e3fdd6d5140437a9af842fa
RubenSH51 <rubencuello51@gmail.com>
2022-01-25 20:49:06 -0300

    Primer commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+print(f"{MESSAGE_COLOR}Almost done!")
+print(f"Initializing a git repository...{RESET_ALL}")
+
+#subprocess.call(['touch','requirements.txt'])
+#subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+
+print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
+
